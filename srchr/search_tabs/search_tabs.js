d27595d3066f2a57ed5ebda95e937715d0831de0
Jeremy Kahn <jeremyckahn@gmail.com>
2010-09-29 13:42:48 -0500

    Beautified, JSLinted and documented the search_tabs plugin.

--- srchr/search_tabs/search_tabs.js
+++ srchr/search_tabs/search_tabs.js
@@ -3,23 +3,35 @@
 	/**
 	 * This plugin generates tabs HTML that is compliant with the Srchr Tabs controller.
 	 * It accepts the Srchr.Models object and dynamically creates the tabs HTML inside of the
-	 * UL that it was called on.
+	 * UL that it was called on.  It also creates the content areas that the tabs will operate on.
 	 * 
+	 * @codestart
+&lt;ul id='resultTabs'&gt;&lt;/ul&gt;
+@codeend
+	 * 
+	 * @codestart
+$("#resultTabs").search_tabs(Srchr.Models);
+	 * @codeend
+	 * 
+	 * All this plugin needs to work is HTML and JS as above.  Individual tabs are created except for "Search" since it is the superclass.
+	 * 
+	 * @return jQuery
+	 * @tag plugins, home
 	 */
 $.fn.search_tabs = function(models) {
   
-	var modelsArr = [], lastModel;
+	var model, modelsArr = [], lastModel;
 	
 		for (model in models){
 			
 			// Don't include the parent "Search" class
 			if (model !== 'Search'){
-				modelsArr.push(model)
+				modelsArr.push(model);
 			}
 		}
 	
 	
-	
+	// Go through each model in the array and make a tab for it, then append it to 'this'
 	for (var i = 0; i < modelsArr.length; i++){
 		this.append($('<li>')
 				.html(
@@ -31,8 +43,10 @@ $.fn.search_tabs = function(models) {
 			);
 	}
 	
-	// Looping through again so that the tabs' content divs show up in the same order as the tabs
-	while (lastModel = modelsArr.pop()){
+	// Create the content containers for each respective tab.
+	// Looping through again so that the tabs' content divs show up in the same order as the tabs.
+	// Note:  Added an extra set of parens in the conditional to make JSLint happy.
+	while ((lastModel = modelsArr.pop())){
 		this.after($('<div>', { id : lastModel }));
 	}
 	
