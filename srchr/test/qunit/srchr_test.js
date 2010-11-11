f80804107383a280439971591f5e54c5c61731e4
Pinhook <contact@pinhooklabs.com>
2010-11-10 22:53:28 -0600

    adding unit tests

--- srchr/test/qunit/srchr_test.js
+++ srchr/test/qunit/srchr_test.js
@@ -1,5 +1,33 @@
-module("srchr");
+module("srchr/models");
 
-test("srchr testing works", function() {
-	ok(true, "an assert is run");
+test("Flickr", function() {
+	stop();
+	Srchr.Models.Flickr.findAll({query: "cats"}, function(data){
+		ok(data, "we got data from flickr");
+		start();
+	})
+});
+
+test("Twitter", function() {
+	stop();
+	Srchr.Models.Twitter.findAll({query: "cats"}, function(data){
+		ok(data, "we got data from Twitter");
+		start();
+	})
+});
+
+test("Upcoming", function() {
+	stop();
+	Srchr.Models.Upcoming.findAll({query: "cats"}, function(data){
+		ok(data, "we got data from Upcoming");
+		start();
+	})
+});
+
+test("Yahoo", function() {
+	stop();
+	Srchr.Models.Yahoo.findAll({query: "cats"}, function(data){
+		ok(data, "we got data from Yahoo");
+		start();
+	})
 });
