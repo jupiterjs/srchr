35f05e4f2b84d01625cc7cef224cb1810bb9926f
Roel Monnens <roelmonnens@gmail.com>
2011-11-09 14:24:31 +0100

    old steal & steal.js in rhino doesn't exist. Must be rhino.js

--- srchr/scripts/build.js
+++ srchr/scripts/build.js
@@ -1,6 +1,6 @@
 //steal/js srchr/scripts/compress.js
-load("steal/rhino/steal.js");
-steal.plugins('steal/build', 'steal/build/scripts', 'steal/build/styles', function() {
+load("steal/rhino/rhino.js");
+steal('steal/build', 'steal/build/scripts', 'steal/build/styles', function() {
 	steal.build('srchr/srchr.html', {
 		to: 'srchr'
 	});
